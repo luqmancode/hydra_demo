8b1fa2246bd0ca67e74f377901f01314ff764722
luqmancode <luqmandepy@gmail.com>
2024-07-26 19:39:23 +0530

    Hydra with configs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from omegaconf import OmegaConf, DictConfig
 import hydra 
 
-@hydra.main(config_path=None)
+@hydra.main(config_path='.', config_name='config')
 def main(config: DictConfig) -> None:
     res = OmegaConf.to_yaml(config)
     print(res)

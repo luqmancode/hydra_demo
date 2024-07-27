0e54b10362d920490484cd56be7522c728c691f0
luqmancode <luqmandepy@gmail.com>
2024-07-27 16:24:09 +0530

    Logging setup

--- 02ConfigProject/main.py
+++ 02ConfigProject/main.py
@@ -2,6 +2,9 @@ from omegaconf import OmegaConf, DictConfig
 import hydra
 import os
 from hydra.utils import get_original_cwd, to_absolute_path
+import logging
+
+logger = logging.getLogger(__name__)
 
 @hydra.main(config_path='.', config_name='config')
 def main(config: DictConfig):
@@ -9,6 +12,8 @@ def main(config: DictConfig):
     print("CURRENT_WORKING_DIRECTORY: ", os.getcwd())
     print("BASE PATH: ", get_original_cwd())
     print("File Path(some.txt): ", to_absolute_path('some.txt'))
+    logger.info("Some Log message")
+    logger.debug("Some debug message")
 
 if __name__ == "__main__":
     main()
